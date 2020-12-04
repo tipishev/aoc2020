043b7b478bfa21dcb84dde5f91cb359e4980c746
Timofey Tipishev <tipishev@gmail.com>
2020-12-04 11:24:28 +0100

    added passport validator for part 1

--- src/day4.erl
+++ src/day4.erl
@@ -1,7 +1,10 @@
 -module(day4).
 
 -export([solve_part1/1, solve_part2/1]).
--export([]).
+-export([parse/1, validate/1]).
+
+% LYSE suggests to keep records private to modules, no header files
+-record(passport, {byr, iyr, eyr, hgt, hcl, ecl, pid, cid}).
 
 %%% solution
 
@@ -10,3 +13,50 @@ solve_part1(_Input) ->
 
 solve_part2(_Input) ->
     undefined.
+
+%%% internals
+
+parse(InputData) ->
+    % InputData.
+    Csv = re:replace(InputData, "\n\n", ",", [global, {return, list}]),
+    WithoutNewlines = re:replace(Csv, "\n", " ", [global, {return, list}]),
+    Lines = string:lexemes(WithoutNewlines, ","),
+    lists:map(fun line_to_passport/1, Lines).
+
+line_to_passport(Line) ->
+    ColonSeps = string:lexemes(Line, " "),
+    Tuples = lists:map(fun colon_sep_to_tuple/1, ColonSeps),
+    tuples_to_passport(Tuples).
+
+colon_sep_to_tuple(Lexeme) ->
+    list_to_tuple(string:split(Lexeme, ":")).
+
+tuples_to_passport(Tuples) ->
+    tuples_to_passport(Tuples, #passport{}).
+
+tuples_to_passport([], Passport) -> Passport;
+tuples_to_passport([{Key, Value} | Tail], Passport) ->
+    NewPassport = case list_to_existing_atom(Key) of
+        byr -> Passport#passport{byr=Value};
+        iyr -> Passport#passport{iyr=Value};
+        eyr -> Passport#passport{eyr=Value};
+        hgt -> Passport#passport{hgt=Value};
+        hcl -> Passport#passport{hcl=Value};
+        ecl -> Passport#passport{ecl=Value};
+        pid -> Passport#passport{pid=Value};
+        cid -> Passport#passport{cid=Value}
+    end,
+    tuples_to_passport(Tail, NewPassport).
+
+%%% part1
+
+validate(#passport{byr=Byr, iyr=Iyr, eyr=Eyr, hgt=Hgt,
+                   hcl=Hcl, ecl=Ecl, pid=Pid, cid=_})  when Byr =/= undefined,
+                                                            Iyr =/= undefined,
+                                                            Eyr =/= undefined,
+                                                            Hgt =/= undefined,
+                                                            Hcl =/= undefined,
+                                                            Ecl =/= undefined,
+                                                            Pid =/= undefined ->
+    true;
+validate(_) -> false.
--- test/day4_tests.erl
+++ test/day4_tests.erl
@@ -1,9 +1,64 @@
 -module(day4_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-part1_test_() ->
+-define(PASSPORTS,
+"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in").
+
+parse_test_() ->
     [
 
-     {"foo",
-     ?_assertEqual(1,1)}
+     {"Simple parse",
+      ?_assertEqual(
+         [
+          {passport,"1937","2017","2020","183cm","#fffffd","gry",
+           "860033327","147"},
+          {passport,"1929","2013","2023",undefined,"#cfa07d","amb",
+           "028048884","350"},
+          {passport,"1931","2013","2024","179cm","#ae17e1","brn",
+           "760753108",undefined},
+          {passport,undefined,"2011","2025","59in","#cfa07d","brn",
+           "166559648",undefined}
+         ]
+         , day4:parse(?PASSPORTS))}
+    ].
+
+validate_test_() ->
+    [
+
+     {"The first passport is valid - all eight fields are present.",
+      ?_assert(day4:validate(
+                 {passport,"1937","2017","2020","183cm",
+                  "#fffffd","gry", "860033327","147"}))},
+
+     {"The second passport is invalid - it is missing hgt (the Height field).",
+      ?_assertNot(day4:validate(
+                 {passport,"1929","2013","2023",undefined,
+                  "#cfa07d","amb", "028048884","350"}))},
+
+     {"The third passport is interesting; the only missing field is cid,
+        so it looks like data from North Pole Credentials,
+        not a passport at all! Surely, nobody would mind if you 
+        made the system temporarily ignore missing cid fields.
+        Treat this 'passport' as valid.",
+      ?_assert(day4:validate(
+                 {passport,"1931","2013","2024","179cm",
+                  "#ae17e1","brn","760753108",undefined}))},
+
+     {"The fourth passport is missing two fields, cid and byr.
+        Missing cid is fine, but missing any other field is not,
+        so this passport is invalid.",
+      ?_assertNot(day4:validate({passport,undefined,"2011","2025","59in","#cfa07d","brn",
+                                "166559648",undefined}))}
     ].
