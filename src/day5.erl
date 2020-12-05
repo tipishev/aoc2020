3c15b6c48253d9bcd425f6a0cf8a382e3ed80de8
Timofey Tipishev <tipishev@gmail.com>
2020-12-05 06:15:25 +0100

    add day5 stub

---
+++ src/day5.erl
@@ -0,0 +1,14 @@
+-module(day5).
+
+-export([solve_part1/1, solve_part2/1]).
+
+% for tests
+-export([to_row_col/1]).
+
+%%% solution
+
+solve_part1(_Input) -> undefined.
+solve_part2(_Input) -> undefined.
+
+%%% internals
+to_row_col(_) -> {70, 7}.
