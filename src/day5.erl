e1e08a3c1417bb17c54ab253bffb1e583de4e7f3
Timofey Tipishev <tipishev@gmail.com>
2020-12-05 06:48:35 +0100

    make halving work

--- src/day5.erl
+++ src/day5.erl
@@ -3,7 +3,7 @@
 -export([solve_part1/1, solve_part2/1]).
 
 % for tests
--export([to_row_col/1]).
+-export([to_row_col/1, half/2]).
 
 %%% solution
 
@@ -11,4 +11,18 @@ solve_part1(_Input) -> undefined.
 solve_part2(_Input) -> undefined.
 
 %%% internals
-to_row_col(_) -> {70, 7}.
+
+half(lower, {Low, High}) ->
+    {Low, Low + (High - Low + 1) div 2 - 1};
+half(upper, {Low, High}) ->
+    {Low + (High - Low + 1) div 2, High}.
+
+
+to_row_col(_) ->
+    to_row_col({70, 7}).
+
+% to_row_col(BinCode) ->
+    % to_row_col(BinCode, {0, 127}, {0, 7}).
+
+% to_row_col(BinCode=[$F|Tail], RowRange, ColRange) when length(BinCode) > 3 -> 42.
+
--- test/day5_tests.erl
+++ test/day5_tests.erl
@@ -1,6 +1,29 @@
 -module(day5_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+range_halve_test_() ->
+    [
+
+     {"Pick lower range (small)",
+      ?_assertEqual({0, 1}, day5:half(lower, {0, 3}))},
+
+     {"Pick upper range (big)",
+      ?_assertEqual({2, 3}, day5:half(upper, {0, 3}))},
+
+     {"Pick lower range (big)",
+      ?_assertEqual({0, 63}, day5:half(lower, {0, 127}))},
+
+     {"Pick upper range (big)",
+      ?_assertEqual({64, 127}, day5:half(upper, {0, 127}))},
+
+     {"Lower, mid-range",
+      ?_assertEqual({32, 47}, day5:half(lower, {32, 63}))},
+
+     {"Upper, mid-range",
+      ?_assertEqual({44, 47}, day5:half(upper, {40, 47}))}
+
+    ].
+
 binary_to_row_col_test_() ->
     [
 
