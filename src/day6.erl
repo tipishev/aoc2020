fbc33becfae8300d83096b3e36e577e30d1b0674
Timofey Tipishev <tipishev@gmail.com>
2020-12-06 07:03:00 +0100

    Makeup? Shucks, no, I woke up like this :)

--- src/day6.erl
+++ src/day6.erl
@@ -2,44 +2,43 @@
 
 -export([solve_part1/1, solve_part2/1]).
 
-% for tests
--export([]).
-
 %%% solution
 
 solve_part1(Input) ->
-    Parsed = parse(Input),
-    Deduplicated = lists:map(fun deduplicate/1, Parsed),
-    lists:sum(lists:map(fun length/1, Deduplicated)).
+    Deduplicated = lists:map(fun deduplicate/1,  parse(Input)),
+    sum_lengths(Deduplicated).
 
 solve_part2(Input) ->
-    Groups = parse2(Input),
-    Intersections = lists:map(fun group_intersection/1, Groups),
-    lists:sum(lists:map(fun length/1, Intersections)).
+    Intersections = lists:map(fun group_intersection/1, parse2(Input)),
+    sum_lengths(Intersections).
 
 %%% internals
+
 parse(Input) ->
-    Csv = re:replace(Input, "\n\n", ",", [global, {return, list}]),
+    Csv = empty_lines_to_commas(Input),
     WithoutNewlines = re:replace(Csv, "\n", " ", [global, {return, list}]),
     WithSpaces = string:lexemes(WithoutNewlines, ","),
-    WithoutSpaces = lists:map(fun remove_spaces/1, WithSpaces),
-    WithoutSpaces.
+    _WithoutSpaces = lists:map(fun drop_spaces/1, WithSpaces).
 
 parse2(Input) ->
-    Csv = re:replace(Input, "\n\n", ",", [global, {return, list}]),
+    Csv = empty_lines_to_commas(Input),
     GroupStrings = string:lexemes(Csv, ","),
-    Groups = lists:map(fun(Str) -> string:lexemes(Str, "\n") end,
-                       GroupStrings),
-    Groups.
+    _Groups = lists:map(fun(Str) -> string:lexemes(Str, "\n") end,
+                        GroupStrings).
 
+%% finds a list of elements shared by all group members
 group_intersection(Group) ->
     Sets = lists:map(fun sets:from_list/1, Group),
-    Intersection = sets:intersection(Sets),
-    sets:to_list(Intersection).
+    sets:to_list(sets:intersection(Sets)).
 
-deduplicate(StringWithRepetitions) ->
-    Sets = sets:from_list(StringWithRepetitions),
-    sets:to_list(Sets).
+deduplicate(List) ->
+    sets:to_list(sets:from_list(List)).
 
-remove_spaces(String) ->
+drop_spaces(String) ->
     re:replace(String, " ", "", [global, {return, list}]).
+
+empty_lines_to_commas(String) ->
+    re:replace(String, "\n\n", ",", [global, {return, list}]).
+
+sum_lengths(List) ->
+    lists:sum(lists:map(fun length/1, List)).
