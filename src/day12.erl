fc246bf8e731b069a73f0afe030bbfa10228a604
Timofey Tipishev <tipishev@gmail.com>
2020-12-23 23:40:27 +0000

    solve day12 part 1

--- src/day12.erl
+++ src/day12.erl
@@ -3,17 +3,18 @@
 -export([solve_part1/1, solve_part2/1]).
 
 % for tests
--export([parse/1]).
+-export([parse/1, cruise/3, turn/2]).
 
 %%% solution
 
-solve_part1(_Input) ->
-    undefined.
+solve_part1(Input) ->
+    {{Lon, Lat}, _Heading} = cruise({0, 0}, 0, parse(Input)),
+    abs(Lon) + abs(Lat).
 
 solve_part2(_Input) ->
     undefined.
 
-%%% parse
+%%% Parsing
 
 parse(Instructions) ->
     [{instruction_to_atom(H), list_to_integer(T)} ||
@@ -21,11 +22,49 @@ parse(Instructions) ->
 
 instruction_to_atom(Char) ->
     case Char of
-        $N -> n;
-        $S -> s;
-        $E -> e;
-        $W -> w;
-        $L -> l;
-        $R -> r;
-        $F -> f
+        $N -> north;
+        $S -> south;
+        $E -> east;
+        $W -> west;
+        $L -> left;
+        $R -> right;
+        $F -> forward
+    end.
+
+%%% Movement
+
+cruise(InitialPosition, InitialHeading, Instructions) ->
+    lists:foldl(fun(Instruction, {Position, Heading}) ->
+                        advance(Position, Heading, Instruction) end,
+                        {InitialPosition, InitialHeading},
+                        Instructions).
+
+% turn
+advance(Position, Heading, {left, Degrees}) ->
+    {Position, turn(Heading, Degrees)};
+advance(Position, Heading, {right, Degrees}) ->
+    {Position, turn(Heading, -Degrees)};
+% absolute shift
+advance({East, North}, Heading, {north, Units}) ->
+    {{East, North + Units}, Heading};
+advance({East, North}, Heading, {south, Units}) ->
+    {{East, North - Units}, Heading};
+advance({East, North}, Heading, {east, Units}) ->
+    {{East + Units, North}, Heading};
+advance({East, North}, Heading, {west, Units}) ->
+    {{East - Units, North}, Heading};
+% relative shift
+advance(Position, Heading, {forward, Units}) ->
+    advance(Position, Heading, {heading_to_direction(Heading), Units}).
+
+%% @doc Turns a given heading by Angle degrees, returns a value in [0, 360].
+turn(Heading, Angle) ->
+    (((Heading + Angle) rem 360) + 360) rem 360.
+
+heading_to_direction(Heading) ->
+    case Heading of
+        0 -> east;
+        90 -> north;
+        180 -> west;
+        270 -> south
     end.
--- test/day12_tests.erl
+++ test/day12_tests.erl
@@ -12,7 +12,39 @@ parse_test_() ->
     [
 
      {"Parse example instructions.",
-      ?_assertEqual([{f, 10}, {n, 3}, {f, 7}, {r, 90}, {f, 11}],
+      ?_assertEqual([{forward, 10}, {north, 3}, {forward, 7},
+                     {right, 90}, {forward, 11}],
                     day12:parse(?EXAMPLE_INSTRUCTIONS))}
 
+    ].
+
+cruise_test_() ->
+    [
+
+     {"Cruise using example instructions, should arrive at {17, -8}, heading South",
+      ?_assertEqual({{17, -8}, 270},
+                    day12:cruise({0, 0}, 0, day12:parse(?EXAMPLE_INSTRUCTIONS)))}
+
+    ].
+
+turn_test_() ->
+    [
+
+     {"Turn +90 from East should head North",
+      ?_assertEqual(90, day12:turn(0, 90))},
+
+     {"Turn -90 from East should head South",
+      ?_assertEqual(270, day12:turn(0, 270))},
+
+     {"Turn 180 from East should head West",
+      ?_assertEqual(180, day12:turn(0, 180))},
+
+     {"Turn 0 from East should head East",
+      ?_assertEqual(0, day12:turn(0, 0))},
+
+     {"Turn 360 from East should head East",
+      ?_assertEqual(0, day12:turn(0, 360))},
+
+     {"Turn 540 from East should head West",
+      ?_assertEqual(180, day12:turn(0, 540))}
     ].
