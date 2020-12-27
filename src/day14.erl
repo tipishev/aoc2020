213ca6f03101bb612682ab71187d44a30bda6bfc
Timofey Tipishev <tipishev@gmail.com>
2020-12-27 08:21:31 +0000

    add parsing

--- src/day14.erl
+++ src/day14.erl
@@ -2,6 +2,8 @@
 
 -export([solve_part1/1, solve_part2/1]).
 
+-export([parse/1]).
+
 %%% solution
 
 solve_part1(_Input) ->
@@ -9,3 +11,25 @@ solve_part1(_Input) ->
 
 solve_part2(_Input) ->
     undefined.
+
+%%% Parse
+parse(Input) ->
+    Lines = split(Input, "\n"),
+    [parse(line, Line) || Line <- Lines].
+
+parse(line, Line) ->
+    [Command | Args] = split(Line, " =[]"),
+    if
+        Command =:= "mask" ->
+           [Mask] = Args,
+           {mask, Mask};
+        Command =:= "mem" ->
+            [Address, Value] = [to_int(Arg) || Arg <- Args],
+            {mem, Address, Value}
+    end.
+
+
+%%% Herlpers
+split(Str, Sep) -> string:lexemes(Str, Sep).
+to_int(Str) -> list_to_integer(Str).
+
--- test/day14_tests.erl
+++ test/day14_tests.erl
@@ -1,12 +1,23 @@
 -module(day14_tests).
 -include_lib("eunit/include/eunit.hrl").
 
--define(EXAMPLE, "").
+-define(EXAMPLE,
+"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0").
 
 parse_test_() ->
     [
 
-     {"Example test",
-      ?_assertEqual(undefined, day14:solve_part1(?EXAMPLE))}
+     {"Parse example program.",
+      ?_assertEqual(
+         [
+          {mask, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"},
+          {mem, 8, 11},
+          {mem, 7, 101},
+          {mem, 8, 0}
+         ],
+         day14:parse(?EXAMPLE))}
 
     ].
