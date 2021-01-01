67b93a4f06fd07ee7ad6e7287c9aa283fcefc8f0
Timofey Tipishev <tipishev@gmail.com>
2021-01-01 20:55:58 +0100

    add a broken test for turns apart case

--- src/day15.erl
+++ src/day15.erl
@@ -2,7 +2,7 @@
 
 -export([solve_part1/1, solve_part2/1]).
 
--export([parse/1, init_memory/1]).
+-export([parse/1, spoken/2]).
 
 %%% solution
 
@@ -12,10 +12,24 @@ solve_part1(_Input) ->
 solve_part2(_Input) ->
     undefined.
 
-
 %%% Parse
 
 parse(Input) ->
     [list_to_integer(N) || N <- string:lexemes(Input, ",")].
 
 %%% Solve Part 1
+
+spoken(Starting, Turn) ->
+    spoken(Starting, Turn, _Mrsn=undefined, _Mem=#{}).
+
+%% @doc Mrsn is Most Recently Spoken Number
+spoken(Starting, Turn, _Mrsn, _Mem)
+  when Turn =< length(Starting)->
+    lists:nth(Turn, Starting);
+
+spoken(_Starting, _Turn, Mrsn, Mem) ->
+    Spoken = case maps:is_key(Mrsn, Mem) of
+        true -> todo;
+        false -> 0
+    end,
+    Spoken.
--- test/day15_tests.erl
+++ test/day15_tests.erl
@@ -1,12 +1,26 @@
 -module(day15_tests).
 -include_lib("eunit/include/eunit.hrl").
 
--define(EXAMPLE, "0,3,6").
-
 parse_test_() ->
     [
 
      {"Example parsing test.",
-      ?_assertEqual([0, 3, 6], day15:parse(?EXAMPLE))}
+      ?_assertEqual([0, 3, 6], day15:parse("0,3,6"))}
 
+    ].
+
+number_spoken_test_() ->
+    [
+
+     {"First Starting Number is spoken first.",
+      ?_assertEqual(0, day15:spoken([0, 3, 6], 1))}
+
+     ,{"Second Starting Number is spoken second.",
+      ?_assertEqual(3, day15:spoken([0, 3, 6], 2))}
+
+     ,{"If Most Recently Spoken Number (MRSN) is new, spoken 0.",
+      ?_assertEqual(0, day15:spoken([0, 3, 6], 4))}
+
+     ,{"If the MRSN is old, next is turns apart.",
+      ?_assertEqual(3, day15:spoken([0, 3, 6], 5))}
     ].
